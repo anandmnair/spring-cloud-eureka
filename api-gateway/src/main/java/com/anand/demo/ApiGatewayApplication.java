fd2355d26fca9d006d39e855d6b0ef63684e5578
Anand Manissery <anand.manissery@gmail.com>
2017-02-08 20:26:46 +0530

    spring cloud with eureka

---
+++ api-gateway/src/main/java/com/anand/demo/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.anand.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
---
+++ cpm-micro-service/src/main/java/com/anand/demo/CpmMicroServiceApplication.java
@@ -0,0 +1,14 @@
+package com.anand.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CpmMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpmMicroServiceApplication.class, args);
+	}
+}
